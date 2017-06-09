e21198a9ae813c52bbc6a0b7287ace024a4e524e
Neeraj <sinhaneeraj@hotmail.com>
2017-06-09 15:13:16 -0700

    Update longest-subarray.py

--- longest-subarray.py
+++ longest-subarray.py
@@ -1,24 +1,21 @@
-a= [1, 2, 3, 3, 2, 4 ,6, 7, 9, 6]
+a= [1, 2, 1, 2, 3, 3, 2, 4 ,6, 7, 9, 6, 2, 3, 6, 7, 8 ]
 print a
 
 def lis(ar):
   b={}
-  b[1] = 1
+  b[0] = 1
   n=len(ar)
-  for i in range(2,n):
+  for i in range(1,n):
     if ar[i] > ar[i-1]:
        b[i]=b[i-1] + 1
     else:
        b[i] = 1
 
-  x=sorted(b.items(), key = lambda x : x[1] , reverse=True)[0]
-  #print x
-  #start=x[0]-x[1]+1
-  #end=x[0]
-  #print ar[start:end+1]
-  p,l = x[0],x[1]
-  print ar[p-l+1:p+1]
+  l = max(b.values())
+  pos = [k for k, v in b.items() if v == l]
+  print l,
+  print pos
+  for p in pos:
+    print ar[p-l+1:p+1]
 
-#print subarray(a)
 lis(a)
-~
